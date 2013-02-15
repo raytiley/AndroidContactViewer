01f3fb7df5ebae5efe1faf1739f224135804f45d
Ray Tiley <raytiley@gmail.com>
2013-02-15 15:51:55 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Android Contact Viewer
+
+This project is a simple contact viewer application for a mobile development class.
+
+
---
+++ src/com/example/AndroidContactViewer/ContactListActivity.java
@@ -0,0 +1,15 @@
+package com.example.AndroidContactViewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ContactListActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

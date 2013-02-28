06355bcc1bf499ebd3fa9f2ede15d987cd15d856
Ray Tiley <raytiley@gmail.com>
2013-02-27 20:21:00 -0600

    Don't populate database on opening the store

--- src/com/example/AndroidContactViewer/datastore/ContactDataSource.java
+++ src/com/example/AndroidContactViewer/datastore/ContactDataSource.java
@@ -40,68 +40,6 @@ public class ContactDataSource implements ContactRepositoryInterface {
 
 	public void open() throws SQLException {
 		database = dbHelper.getWritableDatabase();
-
-		// populate database with dummy contacts
-		if (count() == 0) {
-			List<Contact> _contacts = new ArrayList<Contact>();
-			_contacts.add(new Contact(0, "Malcom Reynolds")
-					.setDefaultEmail("mal@serenity.com").setTitle("Captain")
-					.setDefaultContactPhone("612-555-1234")
-					.setDefaultTextPhone("612-555-1234")
-					.setTwitterId("malcomreynolds"));
-			_contacts.add(new Contact(1, "Zoe Washburne")
-					.setDefaultEmail("zoe@serenity.com").setTitle("First Mate")
-					.setDefaultContactPhone("612-555-5678")
-					.setDefaultTextPhone("612-555-5678")
-					.setTwitterId("zoewashburne"));
-			Contact steve = new Contact(9, "Steven McAdams")
-					.setDefaultEmail("steven.mcadams@lmco.com")
-					.setTitle("Steve")
-					.setDefaultContactPhone("123-456-9874")
-					.setDefaultTextPhone("321-654-6541");
-			steve.addEmail("smcadams86@gmail.com");
-			steve.addPhoneNumber("645-612-6548");
-			_contacts.add(steve);
-
-			_contacts.add(new Contact(2, "Hoban Washburne")
-					.setDefaultEmail("wash@serenity.com").setTitle("Pilot")
-					.setDefaultContactPhone("612-555-9012")
-					.setDefaultTextPhone("612-555-9012").setTwitterId("wash"));
-			_contacts.add(new Contact(3, "Jayne Cobb")
-					.setDefaultEmail("jayne@serenity.com").setTitle("Muscle")
-					.setDefaultContactPhone("612-555-3456")
-					.setDefaultTextPhone("612-555-3456")
-					.setTwitterId("heroofcanton"));
-			_contacts
-					.add(new Contact(4, "Kaylee Frye")
-							.setDefaultEmail("kaylee@serenity.com")
-							.setTitle("Engineer")
-							.setDefaultContactPhone("612-555-7890")
-							.setDefaultTextPhone("612-555-7890")
-							.setTwitterId("kaylee"));
-			_contacts.add(new Contact(5, "Simon Tam")
-					.setDefaultEmail("simon@serenity.com").setTitle("Doctor")
-					.setDefaultContactPhone("612-555-4321")
-					.setDefaultTextPhone("612-555-4321")
-					.setTwitterId("simontam"));
-			_contacts.add(new Contact(6, "River Tam")
-					.setDefaultEmail("river@serenity.com")
-					.setTitle("Doctor's Sister")
-					.setDefaultContactPhone("612-555-8765")
-					.setDefaultTextPhone("612-555-8765")
-					.setTwitterId("miranda"));
-			_contacts.add(new Contact(7, "Shepherd Book")
-					.setDefaultEmail("shepherd@serenity.com")
-					.setTitle("Shepherd")
-					.setDefaultContactPhone("612-555-2109")
-					.setDefaultTextPhone("612-555-2109")
-					.setTwitterId("shepherdbook"));
-			_contacts.add(new Contact(8, "Basic Contact"));
-
-			for (Contact c : _contacts) {
-				add(c);
-			}
-		}
 	}
 
 	public void close() {
